29387b9803c9db6d4afc3a4bc327dc647c1d9950
saurabhkumar2110 <65051413+saurabhkumar2110@users.noreply.github.com>
2020-08-03 15:08:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a = prompt("Enter UserName");
 var b = prompt("Enter password");
-if (a == "Mylove" && b == "Trishu") {
+if (a == "Manan" && b == "Manan") {
     window.location.href = "combined.html";
 } else {
     alert("invalid details");
